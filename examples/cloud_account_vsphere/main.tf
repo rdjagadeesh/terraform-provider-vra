b24453e79ce09f8be7fd9a2bf252669aa01236fa
Deepak Mettem <dmettem@vmware.com>
2021-01-24 23:36:52 -0800

    Fix misisng fields while importing resources
    This commit fixes the missing fields while the resources are imported for
    resources such as vra_cloud_account_vsphere, vra_zone, vra_flavor_profile,
    vra_network_profile, and vra_storage_profile.
    
    Signed-off-by: Deepak Mettem <dmettem@vmware.com>

--- examples/cloud_account_vsphere/main.tf
+++ examples/cloud_account_vsphere/main.tf
@@ -5,34 +5,34 @@ provider "vra" {
 }
 
 // Required for vRA Cloud, Optional for vRA 8.X
-data "vra_data_collector" "dc" {
-  count = var.datacollector != "" ? 1 : 0
-  name  = var.datacollector
-}
-
-resource "vra_cloud_account_nsxt" "this" {
-  name        = "tf-nsx-t-account"
-  description = "foobar"
-  username    = var.nsxt_username
-  password    = var.nsxt_password
-  hostname    = var.nsxt_hostname
-  dc_id       = var.datacollector != "" ? data.vra_data_collector.dc[0].id : "" // Required for vRA Cloud, Optional for vRA 8.X
-
-  accept_self_signed_cert = true
-
-  tags {
-    key   = "foo"
-    value = "bar"
-  }
-}
-
-data "vra_region_enumeration_vsphere" "this" {
-  username                = var.username
-  password                = var.password
-  hostname                = var.hostname
-  dcid                    = var.datacollector != "" ? data.vra_data_collector.dc[0].id : "" // Required for vRA Cloud, Optional for vRA 8.X
-  accept_self_signed_cert = true
-}
+//data "vra_data_collector" "dc" {
+//  count = var.datacollector != "" ? 1 : 0
+//  name  = var.datacollector
+//}
+//
+//resource "vra_cloud_account_nsxt" "this" {
+//  name        = "tf-nsx-t-account"
+//  description = "foobar"
+//  username    = var.nsxt_username
+//  password    = var.nsxt_password
+//  hostname    = var.nsxt_hostname
+//  dc_id       = var.datacollector != "" ? data.vra_data_collector.dc[0].id : "" // Required for vRA Cloud, Optional for vRA 8.X
+//
+//  accept_self_signed_cert = true
+//
+//  tags {
+//    key   = "foo"
+//    value = "bar"
+//  }
+//}
+//
+//data "vra_region_enumeration_vsphere" "this" {
+//  username                = var.username
+//  password                = var.password
+//  hostname                = var.hostname
+//  dcid                    = var.datacollector != "" ? data.vra_data_collector.dc[0].id : "" // Required for vRA Cloud, Optional for vRA 8.X
+//  accept_self_signed_cert = true
+//}
 
 resource "vra_cloud_account_vsphere" "this" {
   name        = "tf-vsphere-account"
@@ -42,8 +42,8 @@ resource "vra_cloud_account_vsphere" "this" {
   hostname    = var.hostname
   dcid        = var.datacollector != "" ? data.vra_data_collector.dc[0].id : "" // Required for vRA Cloud, Optional for vRA 8.X
 
-  regions                      = data.vra_region_enumeration_vsphere.this.regions
-  associated_cloud_account_ids = [vra_cloud_account_nsxt.this.id]
+//  regions                      = data.vra_region_enumeration_vsphere.this.regions
+//  associated_cloud_account_ids = [vra_cloud_account_nsxt.this.id]
   accept_self_signed_cert      = true
 
   tags {
